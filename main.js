28b3cd33121b36161715600bc20ef37838afe6de
d3n2el <lialindaniel@gmail.com>
2025-08-06 18:12:42 +0200

    made it so it receives user input, it is stuck on 10 minutes timer forever though

--- main.js
+++ main.js
@@ -3,22 +3,29 @@ const pauseButton = document.getElementById('pause-btn');
 const ResetButton = document.getElementById('reset-btn');
 const timerEndSound = new Audio('vine.mp3');
 let timer;
-let initialTime = 25*60
-let timeLeft = initialTime /* will need to add user input later, now just making a timer that works*/
+let initialMinutes = document.getElementById('user-minutes');
+const initialSeconds = document.getElementById('user-seconds');
+let initialTime = parseInt(initialMinutes.value) * 60 + parseInt(initialSeconds.value);
+let timeLeft = initialTime;
 // change text content automatically as time passes
-function updateDisplay() {
+function updateMinutesDisplay() {
     const minutes = Math.floor(timeLeft/60);
-    const seconds = timeLeft % 60;
-    document.querySelector('.time').textContent =
-        `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    document.getElementById('user-minutes').value =
+        `${minutes.toString().padStart(2, '0')}`;
 
+}
+function updateSecondsDisplay() {
+    const seconds = timeLeft % 60;
+    document.getElementById('user-seconds').value =
+        `${seconds.toString().padStart(2, '0')}`
 }
 
 function startTimer() {
     if(timer) clearInterval(timer);
     timer = setInterval(() => {
         timeLeft--;
-        updateDisplay();
+        updateMinutesDisplay();
+        updateSecondsDisplay();
         if(timeLeft<=0){
             clearInterval(timer);
             alert("wake up my G, it's time to take a break. You did good"); // yes, i did put a motivational message here
